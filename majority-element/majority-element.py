084852a02fe2d3031d832c8fac117995927b53cb
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2023-06-25 18:48:05 +0530

    Time: 175 ms (61.92%) | Memory: 18 MB (30.91%) - LeetSync

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,12 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mp = {}
-        for i in nums:
-            if i not in mp:
-                mp[i]=1
+        # Hashing
+
+        # mp = {}
+        # for i in nums:
+        #     if i not in mp:
+        #         mp[i]=1
+        #     else:
+        #         mp[i]+=1
+        # N = len(nums)
+        # for x,y in mp.items():
+        #     if y>N/2:
+        #         return x
+
+        # Moor's Algorithm
+
+        el = 0
+        count = 0
+        for i in range(len(nums)):
+            if count == 0:
+                count += 1
+                el = nums[i]
+            elif el == nums[i]:
+                count +=1
             else:
-                mp[i]+=1
-        N = len(nums)
-        for x,y in mp.items():
-            if y>N/2:
-                return x
+                count -=1
+        return el
