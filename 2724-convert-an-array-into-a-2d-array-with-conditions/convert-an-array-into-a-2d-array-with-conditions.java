90d5abdae11d93462bd0d4160e522f4277e4f7c6
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2024-01-03 03:10:57 +0530

    Time: 3 ms (76.20%) | Memory: 45 MB (10.26%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -4,14 +4,21 @@ class Solution {
         int[] freq = new int[nums.length + 1];
 
         for (int num : nums) {
+            // if the frequency of current
+            // element is greater than or equal
+            // to current number or arrays in ans
+            // array, add a new array in answer.
             if (freq[num] >= ans.size()) {
                 ans.add(new ArrayList<>());
             }
+
+            // add the current element to the appropriate array in ans.
             ans.get(freq[num]).add(num);
+
+            // inclrease the frequency of num.
             freq[num]++;
 
         }
-
         return ans;
 
     }
