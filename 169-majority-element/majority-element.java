aae38bbcab670511fa19fc06790e88f3ce84bd6e
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2024-09-05 00:51:45 +0530

    Time: 13 ms (29.00%) | Memory: 49.6 MB (77.33%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,13 +2,15 @@ class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> hash = new HashMap<>();
         for (int i : nums) {
-            hash.put(i, hash.getOrDefault(i, 0)+1);
+            hash.put(i, hash.getOrDefault(i, 0) + 1);
         }
-        AtomicInteger ans = new AtomicInteger();
-        hash.entrySet().forEach(e -> {
-            if (e.getValue() > nums.length / 2)
-                ans.set(e.getKey());
-        });
-        return ans.get();
+        return Collections.max(hash.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+        // AtomicInteger ans = new AtomicInteger();
+        // hash.entrySet().forEach(e -> {
+        //     if (e.getValue() > nums.length / 2)
+        //         ans.set(e.getKey());
+        // });
+        // return ans.get();
     }
 }
