63b8c2958c19da1c1b6c36fa0436c802e4a1d541
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2024-09-05 19:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (96.72%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,33 +1,30 @@
 class Solution {
     public void rotate(int[][] matrix) {
-
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[i].length; j++){
-                swapMatrix(matrix,i,j, j,i);
-            }
-        }
-
-        for(int i = 0; i<matrix.length; i++){
-            reverse(matrix[i], 0, matrix[i].length-1);
-        }
-        
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = i; j < matrix[i].length; j++)
+                swapMatrix(matrix, i, j, j, i);
+        for (int i = 0; i < matrix.length; i++)
+            reverse(matrix[i], 0, matrix[i].length - 1);
     }
-    void swapMatrix(int[][] arr, int i1, int i2, int j1, int j2){
+
+    void swapMatrix(int[][] arr, int i1, int i2, int j1, int j2) {
         int temp = arr[i1][i2];
         arr[i1][i2] = arr[j1][j2];
         arr[j1][j2] = temp;
     }
-    void reverse(int[] arr, int l, int r){
-        while(l<r){
+
+    void reverse(int[] arr, int l, int r) {
+        while (l < r) {
             swap(arr, l, r);
             l++;
             r--;
         }
     }
-    void swap(int[] arr, int i, int j){
+
+    void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-        
+
     }
 }
