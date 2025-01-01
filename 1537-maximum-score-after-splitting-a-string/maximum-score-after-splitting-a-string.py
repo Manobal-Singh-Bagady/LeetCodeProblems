4ebd2905dcfb524666b6b949c8e230ef0efceeaa
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-01 22:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,13 +1,16 @@
+import math
 class Solution:
     def maxScore(self, s: str) -> int:
-        score = 0
-        ones = s.count('1')
+        ones = 0
         zeros = 0
+        best = -math.inf
         for i in range(len(s)-1):
             if s[i]=='0':
                 zeros+=1
             else:
-                ones-=1
-            score = max(score, ones+zeros)
-        return score
+                ones+=1
+            best = max(best, zeros-ones)
+        if s[-1]=='1':
+            ones+=1
+        return ones+best
         
