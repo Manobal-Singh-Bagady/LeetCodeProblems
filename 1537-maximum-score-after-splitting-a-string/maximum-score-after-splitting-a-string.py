632741091e9c63a93a1bf37aadddb92939ac6e9e
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-01 22:25:09 +0530

    Time: 1 ms (68.37%) | Memory: 17.7 MB (21.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,16 +1,23 @@
 import math
+
+
+# score = O(L) + 1(R)
+# score = 0(L) + 1(T) - 1(L)
+# 1(T) -> Constant
+# 0(L)-1(L) -> Variable
+# score = ones + best
+
 class Solution:
     def maxScore(self, s: str) -> int:
         ones = 0
         zeros = 0
         best = -math.inf
-        for i in range(len(s)-1):
-            if s[i]=='0':
-                zeros+=1
+        for i in range(len(s) - 1):
+            if s[i] == '0':
+                zeros += 1
             else:
-                ones+=1
-            best = max(best, zeros-ones)
-        if s[-1]=='1':
-            ones+=1
-        return ones+best
-        
+                ones += 1
+            best = max(best, zeros - ones)
+        if s[-1] == '1':
+            ones += 1
+        return ones + best
