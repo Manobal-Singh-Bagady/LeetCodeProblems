aa875c4f8d22adde8caff20fc6a9d75ac6a801ba
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-04 21:52:35 +0530

    Time: 103 ms (87.69%) | Memory: 18.2 MB (32.97%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -3,10 +3,6 @@ class Solution:
         ans = 0
         for letter in set(s):
             l, r = s.index(letter)+1, s.rindex(letter)
-            between = set()
-            for i in range(l, r):
-                if s[i] not in between:
-                    ans+=1
-                    between.add(s[i])
+            ans+=len(set(s[l:r]))
         return ans
 
