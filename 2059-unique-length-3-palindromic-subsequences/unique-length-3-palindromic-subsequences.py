ee04aa641362ae365c559433c3b1c7bbd1ffa660
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-04 21:46:23 +0530

    Time: 107 ms (87.47%) | Memory: 18.4 MB (22.86%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,17 +1,8 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        char_indices = dict()
-
-        for i, char in enumerate(s):
-            if char not in char_indices:
-                char_indices[char] = []
-            char_indices[char].append(i)
-
         ans = 0
-        for char_index in char_indices.values():
-            l = char_index[0] + 1
-            r = char_index[-1]
-            if l != r:
-                unique_between = set(s[l:r])
-                ans += len(unique_between)
+        for letter in set(s):
+            l, r = s.index(letter)+1, s.rindex(letter)
+            ans+=len(set(s[l:r]))
         return ans
+
