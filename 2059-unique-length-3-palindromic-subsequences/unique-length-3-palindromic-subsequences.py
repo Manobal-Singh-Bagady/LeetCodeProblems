4e1577e076fcfa8f0595dff96546ed65d31e74f5
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-04 21:40:46 +0530

    Time: 167 ms (77.80%) | Memory: 22.4 MB (7.69%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -9,9 +9,9 @@ class Solution:
 
         ans = 0
         for char_index in char_indices.values():
-            l = char_index[0] + 1
-            r = char_index[-1]
-            if l != r:
+            if len(char_index) > 1:
+                l = char_index[0] + 1
+                r = char_index[-1]
                 unique_between = set(s[l:r])
                 ans += len(unique_between)
         return ans
