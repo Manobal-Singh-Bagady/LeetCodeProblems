82e3618d75abbc47053a28397ad76b1b2fbd075c
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-06 00:17:27 +0530

    Time: 54 ms (86.32%) | Memory: 41.2 MB (49.24%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,6 +1,8 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts) -> str:
         n = len(s)
+
+        # array to keep the range of shifting
         shift_arr=[0]*n
         for l, r, inc in shifts:
             shift_arr[l] += (1 if inc else -1)
@@ -11,13 +13,14 @@ class Solution:
         string = list(s)
         for i in range(n):
             current_shift+=shift_arr[i]
-            current_shift%=26
-            current_chr=ord(string[i])
-            current_chr+=current_shift
-            if current_chr>ord('z'):
-                current_chr-=26
-            if current_chr<ord('a'):
-                current_chr+=26
+            # current_shift%=26
+            # current_chr=ord(string[i])
+            current_chr = ord('a') + (ord(string[i])-ord('a')+current_shift)%26
+            # current_chr+=current_shift
+            # if current_chr>ord('z'):
+            #     current_chr-=26
+            # if current_chr<ord('a'):
+            #     current_chr+=26
             string[i]=chr(current_chr)
         return ''.join(string)
 
