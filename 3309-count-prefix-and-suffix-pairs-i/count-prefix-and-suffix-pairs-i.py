903b07979145b79cae2c994c4d36b60f0f33bd3c
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-08 22:16:09 +0530

    Time: 11 ms (56.86%) | Memory: 17.9 MB (6.47%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -1,11 +1,12 @@
 class Solution:
+    def isPrefixAndSuffix(self, str1, str2):
+        return str2.startswith(str1) and str2.endswith(str1)
+
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         n = len(words)
         count = 0
         for i in range(n):
             for j in range(i + 1, n):
-                str1 = words[i]
-                str2 = words[j]
-                if str2.startswith(str1) and str2.endswith(str1):
+                if self.isPrefixAndSuffix(words[i], words[j]):
                     count += 1
         return count
