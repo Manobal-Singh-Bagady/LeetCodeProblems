1ec46a52bd257ceb21e7c9181bad038c5199c976
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-10 22:33:41 +0530

    Time: 140 ms (94.82%) | Memory: 21.2 MB (32.67%) - LeetSync

--- 952-word-subsets/word-subsets.py
+++ 952-word-subsets/word-subsets.py
@@ -1,13 +1,13 @@
 class Solution:
     def wordSubsets(self, words1, words2):
-        letter_count = dict()
+        count2 = dict()
         for word in words2:
             for letter in word:
-                letter_count[letter] = max(letter_count.get(letter, 0), word.count(letter))
+                count2[letter] = max(count2.get(letter, 0), word.count(letter))
 
         ans = []
         for word in words1:
-            if all(word.count(letter) >= letter_count.get(letter) for letter in letter_count):
+            if all(word.count(letter) >= count2.get(letter) for letter in count2):
                 ans.append(word)
 
         return ans
