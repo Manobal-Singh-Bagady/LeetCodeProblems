9b6e44761b8d5ed1168b3b850bc3f1ede80fe9d9
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-11 21:22:56 +0530

    Time: 19 ms (99.56%) | Memory: 18.1 MB (16.30%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,8 +1,14 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
+        n = len(s)
+        if k == n:
+            return True
+        if k > n:
+            return False
+
         odds = 0
         for i in set(s):
             if s.count(i) % 2:
                 odds += 1
 
-        return odds <= k and k<=len(s)
+        return odds <= k
