3bd38f035b7ea7b8f45e94a3b5aa6a0b54419939
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-11 21:00:06 +0530

    Time: 51 ms (25.11%) | Memory: 18.1 MB (34.36%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -8,5 +8,12 @@ class Solution:
             return True
         if k > n:
             return False
-        map = Counter(s)
-        return sum(1 for i in map if map[i] % 2 != 0) <= k
+        # map = Counter(s)
+        # return sum(1 for i in map if map[i] % 2 != 0) <= k
+
+        freq = [0]*26
+        for char in s:
+            freq[ord(char)-ord('a')]+=1
+
+        odds = sum(1 for i in freq if i%2)
+        return odds<=k
