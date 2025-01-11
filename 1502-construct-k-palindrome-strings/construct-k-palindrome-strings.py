ed53973ca89ee95cd4e099bdbc64f0f3534e484f
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-11 21:47:12 +0530

    Time: 59 ms (14.54%) | Memory: 18.3 MB (8.81%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -6,10 +6,6 @@ class Solution:
             return False
 
         odds = 0
-        for i in set(s):
-            if s.count(i) % 2:
-                odds += 1
-        
-        if odds <= k:
-            return True
-        return False
+        for chr in s:
+            odds^=1<<(ord(chr)-ord('a'))
+        return bin(odds).count('1')<=k
