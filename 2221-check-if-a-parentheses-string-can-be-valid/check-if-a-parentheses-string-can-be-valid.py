3dffdf138029497ed2664dee0a094bf3688fa6d8
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-12 22:29:46 +0530

    Time: 107 ms (52.74%) | Memory: 18.6 MB (57.06%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -3,24 +3,41 @@ class Solution:
         if (n := len(s)) & 1:
             return False
 
-        brackets = []
-        unlocked = []
+        brackets = 0
+        unlocked = 0
 
         for i in range(n):
             if locked[i] == "0":
-                unlocked.append(i)
+                unlocked+=1
             elif s[i] == "(":
-                brackets.append(i)
+                brackets+=1
             elif s[i] == ")":
                 if brackets:
-                    brackets.pop()
+                    brackets-=1
                 elif unlocked:
-                    unlocked.pop()
+                    unlocked-=1
                 else:
                     return False
 
-        while brackets and unlocked and brackets[-1] < unlocked[-1]:
-            brackets.pop()
-            unlocked.pop()
+        balance = 0
+        for i in range(n-1, -1, -1):
+            if locked[i]=='0':
+                balance -=1
+                unlocked-=1
+            elif s[i]=='(':
+                balance+=1
+                brackets-=1
+            elif s[i]==')':
+                balance-=1
 
-        return not brackets
+            if balance>0:
+                return False
+            if unlocked==0 and brackets==0:
+                return True
+        # while brackets and unlocked and brackets[-1] < unlocked[-1]:
+        #     brackets.pop()
+        #     unlocked.pop()
+
+        if brackets>0:
+            return False
+        return True
