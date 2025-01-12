615b409eb3a51fea255081b6acc53fe41eb479c5
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-12 22:17:04 +0530

    Time: 63 ms (91.64%) | Memory: 22 MB (6.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,31 +1,26 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        if (n:=len(s))&1:
+        if (n := len(s)) & 1:
             return False
 
         brackets = []
         unlocked = []
 
         for i in range(n):
-            if locked[i]=='0':
+            if locked[i] == "0":
                 unlocked.append(i)
-            elif s[i] == '(':
+            elif s[i] == "(":
                 brackets.append(i)
-            elif s[i] == ')':
+            elif s[i] == ")":
                 if brackets:
                     brackets.pop()
                 elif unlocked:
                     unlocked.pop()
                 else:
                     return False
-        
-        while brackets and unlocked and brackets[-1]<unlocked[-1]:
+
+        while brackets and unlocked and brackets[-1] < unlocked[-1]:
             brackets.pop()
             unlocked.pop()
-        
-        if brackets:
-            return False
-        return True
 
-
-        
+        return not brackets
