4b00c5112f0d8847ecd97af907cbdcfe43ea94af
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-13 21:58:04 +0530

    Time: 129 ms (70.48%) | Memory: 18.9 MB (14.92%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,9 +1,5 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
         freq = Counter(s)
-        
-        ans = 0
-        for i in freq.values():
-            ans+= 1 if i&1 else 2
-        return ans            
+        return sum(1 if i&1 else 2 for i in freq.values())           
         
