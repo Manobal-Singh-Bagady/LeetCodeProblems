876c604a43f95b0a56eebb9ba86492627e572f35
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-14 22:23:46 +0530

    Time: 4 ms (85.42%) | Memory: 17.8 MB (32.03%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -4,13 +4,13 @@ class Solution:
         count = 0
         ans = []
         for i in range(len(A)):
-            if freq & 1 << A[i]:
+            if freq & 1 << A[i]:  # If A[i] bit is already flipped count+=1
                 count += 1
-            freq ^= 1 << A[i]
+            freq ^= 1 << A[i]  # Flip the A[i] bit of freq
 
-            if freq & 1 << B[i]:
+            if freq & 1 << B[i]:  # If B[i] bit is already flipped count+=1
                 count += 1
-            freq ^= 1 << B[i]
+            freq ^= 1 << B[i]  # Flip the B[i] bit of freq
 
             ans.append(count)
         return ans
