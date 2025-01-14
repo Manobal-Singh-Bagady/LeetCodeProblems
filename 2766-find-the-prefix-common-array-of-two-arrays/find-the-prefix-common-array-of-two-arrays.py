a383b8c406ae47fb4ccbbe7bed2f9d16fabd1a70
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-14 22:33:05 +0530

    Time: 4 ms (85.42%) | Memory: 17.9 MB (9.90%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,16 +1,16 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         freq = 0
-        ans = [0]*(n:=len(A))
-        for i in range(n):
-            if i!=0:
-                ans[i]=ans[i-1]
-            
+        count = 0
+        ans = []
+        for i in range(len(A)):
             if freq & 1 << A[i]:  # If A[i] bit is already flipped count+=1
-                ans[i] += 1
+                count += 1
             freq ^= 1 << A[i]  # Flip the A[i] bit of freq
 
             if freq & 1 << B[i]:  # If B[i] bit is already flipped count+=1
-                ans[i] += 1
+                count += 1
             freq ^= 1 << B[i]  # Flip the B[i] bit of freq
+
+            ans.append(count)
         return ans
