906df9e20bef379e5b79b3b86311d237b45bfdc9
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-14 21:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (45.05%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -5,12 +5,13 @@ class Solution:
         ans = []
         for i in range(n):
             freq[A[i]]+=1
-            freq[B[i]]+=1
-
             if freq[A[i]]==2:
                 count+=1
-            if freq[B[i]]==2 and A[i]!=B[i]:
+            
+            freq[B[i]]+=1
+            if freq[B[i]]==2:
                 count+=1
+                
             ans.append(count)
         return ans
         
