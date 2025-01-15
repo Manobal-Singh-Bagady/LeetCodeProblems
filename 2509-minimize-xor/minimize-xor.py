4c9e534c04d65ddd19f9803b417773c9edf5cc70
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-15 23:33:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.40%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -1,36 +1,32 @@
 class Solution:
     def check_setbit(self, num: int, bit: int) -> bool:
-        return num & 1 << bit
+        return bool(num & 1 << bit)
 
     def flip_bit(self, num: int, bit: int) -> int:
         return num ^ 1 << bit
 
     def minimizeXor(self, num1: int, num2: int) -> int:
-        bin1 = bin(num1)
-        bin2 = bin(num2)
-
-        bits1 = bin1.count("1")
-        bits2 = bin2.count("1")
+        bits1 = bin(num1).count("1")
+        bits2 = bin(num2).count("1")
 
         if bits1 == bits2:
             return num1
-        elif bits2 > bits1:
-            ans = num1
-            n = bits2 - bits1
-            i = 0
-            while n:
-                if not self.check_setbit(ans, i):
-                    ans = self.flip_bit(ans, i)
-                    n -= 1
-                i += 1
-            return ans
-        else:
-            ans = num1
-            n = bits1 - bits2
-            i = 0
-            while n:
-                if self.check_setbit(ans, i):
-                    ans = self.flip_bit(ans, i)
-                    n -= 1
-                i += 1
-            return ans
+        
+        ans = num1
+        n = abs(bits2 - bits1)
+        i = 0
+        check_bit = False if bits2>bits1 else True
+        # if bits2 > bits1:
+        #     while n:
+        #         if not self.check_setbit(ans, i):
+        #             ans = self.flip_bit(ans, i)
+        #             n -= 1
+        #         i += 1
+        #     return ans
+        # else:
+        while n:
+            if self.check_setbit(ans, i)==check_bit:
+                ans = self.flip_bit(ans, i)
+                n -= 1
+            i += 1
+        return ans
