fc9a6135a19c663de6377088353c460a707943cb
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-15 23:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.20%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -6,18 +6,10 @@ class Solution:
         ans = num1
         n = abs(bits2 - bits1)
         i = 0
-        if bits2 > bits1:
-            set_bit = False
-            # while n:
-            #     if not (ans & 1 << i):  # check if bit is unset
-            #         ans ^= 1 << i       # flip unset bit
-            #         n -= 1
-            #     i += 1
-        elif bits1 > bits2:
-            set_bit = True
+        set_bit = False if bits2 > bits1 else True
         while n:
-            if bool(ans & 1 << i)==set_bit:        # check if bit is set
-                ans ^= 1 << i       # flip set bit
+            if bool(ans & 1 << i) == set_bit:
+                ans ^= 1 << i
                 n -= 1
             i += 1
         return ans
