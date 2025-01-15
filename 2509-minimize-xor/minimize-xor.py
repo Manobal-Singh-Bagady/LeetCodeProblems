88427653ea3d11b44408f5d1de63b1a00f010a0e
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-15 23:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (59.20%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -1,36 +1,36 @@
 class Solution:
+    def check_setbit(self, num: int, bit: int) -> bool:
+        return num & 1 << bit
+
+    def flip_bit(self, num: int, bit: int) -> int:
+        return num ^ 1 << bit
+
     def minimizeXor(self, num1: int, num2: int) -> int:
         bin1 = bin(num1)
         bin2 = bin(num2)
 
-        bits1 = bin1.count('1')
-        bits2 = bin2.count('1')
+        bits1 = bin1.count("1")
+        bits2 = bin2.count("1")
 
-        if bits1==bits2:
+        if bits1 == bits2:
             return num1
-        elif bits2>bits1:
+        elif bits2 > bits1:
             ans = num1
-            n = bits2-bits1
+            n = bits2 - bits1
             i = 0
             while n:
-                if not (ans&1<<i):
-                    ans ^= 1<<i
-                    n-=1
-                i+=1
+                if not self.check_setbit(ans, i):
+                    ans = self.flip_bit(ans, i)
+                    n -= 1
+                i += 1
             return ans
         else:
             ans = num1
-            n = bits1-bits2
+            n = bits1 - bits2
             i = 0
             while n:
-                if ans&1<<i:
-                    ans ^= 1<<i
-                    n-=1
-                i+=1
+                if self.check_setbit(ans, i):
+                    ans = self.flip_bit(ans, i)
+                    n -= 1
+                i += 1
             return ans
-
-
-
-                
-        
-        
