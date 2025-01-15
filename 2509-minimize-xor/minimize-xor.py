d42f6cf9708f912e8990a2f3ad582311960357ac
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-15 23:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.40%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -4,15 +4,17 @@ class Solution:
         bits2 = bin(num2).count("1")
 
         ans = num1
-        n = abs(bits2 - bits1)
-        i = 0
         if bits2 > bits1:
+            n = bits2 - bits1
+            i = 0
             while n:
                 if not (ans & 1 << i):
                     ans ^= 1 << i
                     n -= 1
                 i += 1
         elif bits1 > bits2:
+            n = bits1 - bits2
+            i = 0
             while n:
                 if ans & 1 << i:
                     ans ^= 1 << i
