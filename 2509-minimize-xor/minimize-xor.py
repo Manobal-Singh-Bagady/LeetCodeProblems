72ac2ffbd1e398fa22e495fd8804cc3c3c7325f9
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-15 23:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (14.40%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -6,11 +6,8 @@ class Solution:
         return num ^ 1 << bit
 
     def minimizeXor(self, num1: int, num2: int) -> int:
-        bin1 = bin(num1)
-        bin2 = bin(num2)
-
-        bits1 = bin1.count("1")
-        bits2 = bin2.count("1")
+        bits1 = bin(num1).count("1")
+        bits2 = bin(num2).count("1")
 
         if bits1 == bits2:
             return num1
