c7974ba0083b470b2f4ec7cb4b3777faaf08efff
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-20 23:38:42 +0530

    Time: 111 ms (79.49%) | Memory: 44 MB (95.58%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -3,23 +3,20 @@ class Solution:
         rowNum = len(mat)
         colNum = len(mat[0])
 
-        numToPos = [0]*(rowNum*colNum+1)
+        numToPos = [0] * (rowNum * colNum + 1)
         for i in range(rowNum):
             for j in range(colNum):
-                numToPos[mat[i][j]]=i,j
-        
-        rowCount = [0]*rowNum
-        colCount = [0]*colNum
+                numToPos[mat[i][j]] = i, j
 
-        for i in range(rowNum*colNum):
-            x,y = numToPos[arr[i]]
-            rowCount[x]+=1
-            if rowCount[x]==colNum:
+        rowCount = [0] * rowNum
+        colCount = [0] * colNum
+
+        for i in range(rowNum * colNum):
+            x, y = numToPos[arr[i]]
+            rowCount[x] += 1
+            if rowCount[x] == colNum:
                 return i
-            colCount[y]+=1
-            if colCount[y]==rowNum:
+            colCount[y] += 1
+            if colCount[y] == rowNum:
                 return i
         return -1
-
-
-        
