3d5967218f8e96a4795ef033a1c13bebb059338e
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-23 00:07:19 +0530

    Time: 916 ms (59.76%) | Memory: 84.6 MB (73.18%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -1,27 +1,30 @@
 class Solution:
-    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+    _dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    def _isValidCell(self, x, y, row, col):
+        return 0 <= x < row and 0 <= y < col
 
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         row, col = len(isWater), len(isWater[0])
-        ans = [[-1]*col for _ in range(row)]
+        ans = [[-1] * col for _ in range(row)]
         queue = deque()
 
+        # Mark water cells as 0
         for i in range(row):
-            temp = []
             for j in range(col):
-                if isWater[i][j]==1:
-                    ans[i][j]=0
-                    queue.append((i,j))
-        
+                if isWater[i][j] == 1:
+                    ans[i][j] = 0
+                    queue.append((i, j))
+
+        # BFS
         while queue:
-            i,j = queue.popleft()
-            for dx,dy in dirs:
-                x,y = i+dx, j+dy
-                if (0<=x<row and 0<=y<col) and ans[x][y] == -1:
-                    ans[x][y] = ans[i][j]+1
-                    queue.append((x,y))
+            i, j = queue.popleft()
+
+            # Get all neighbouring cells
+            for dx, dy in self._dirs:
+                x, y = i + dx, j + dy
+                # If un-visited and valid
+                if self._isValidCell(x, y, row, col) and ans[x][y] == -1:
+                    # height = current+1
+                    ans[x][y] = ans[i][j] + 1
+                    queue.append((x, y))
         return ans
-
-
-
-
