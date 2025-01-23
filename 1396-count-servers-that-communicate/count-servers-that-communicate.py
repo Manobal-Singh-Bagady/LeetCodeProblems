637036ec93db7bf17881c3d351936f02e283cb10
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-23 20:36:02 +0530

    Time: 10 ms (91.76%) | Memory: 19.8 MB (22.47%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -2,20 +2,18 @@ class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         row = len(grid)
         col = len(grid[0])
-        rowCount = [0]*row
-        colCount = [0]*col
+        rowCount = [0] * row
+        colCount = [0] * col
 
+        q = deque()
         for i in range(row):
             for j in range(col):
-                if grid[i][j]==1:
-                    rowCount[i]+=1
-                    colCount[j]+=1
+                if grid[i][j] == 1:
+                    rowCount[i] += 1
+                    colCount[j] += 1
+                    q.append((i, j))
         ans = 0
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j]==1 and (rowCount[i]>1 or colCount[j]>1):
-                    ans+=1
+        for i, j in q:
+            if rowCount[i] > 1 or colCount[j] > 1:
+                ans += 1
         return ans
-
-
-        
