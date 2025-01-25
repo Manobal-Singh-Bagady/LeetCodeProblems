b97428d2100eec3b7cb64585db775394c353ebc0
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-25 22:33:42 +0530

    Time: 367 ms (50.00%) | Memory: 46.7 MB (84.03%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,10 +1,10 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
-        enum = sorted((num,i) for i, num in enumerate(nums))
+        enum = sorted((num, i) for i, num in enumerate(nums))
         groups = [[enum[0]]]
-        for i in range(1,n):
-            if enum[i][0]-enum[i-1][0]<=limit:
+        for i in range(1, n):
+            if enum[i][0] - enum[i - 1][0] <= limit:
                 groups[-1].append(enum[i])
             else:
                 groups.append([enum[i]])
@@ -12,9 +12,5 @@ class Solution:
             vals = sorted([i[0] for i in group])
             idxs = sorted([i[1] for i in group])
             for i in range(len(group)):
-                nums[idxs[i]]=vals[i]
+                nums[idxs[i]] = vals[i]
         return nums
-
-
-
-        
