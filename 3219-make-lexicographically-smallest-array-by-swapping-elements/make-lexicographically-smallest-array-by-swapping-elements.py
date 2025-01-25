89471eb12941d99f9c4e7e653bf13f4f2ec929cb
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-25 22:58:50 +0530

    Time: 248 ms (90.97%) | Memory: 96.4 MB (5.56%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,15 +1,30 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
-        enum = sorted((nums[i],i) for i in range(n))
-        groups = [[enum[0]]]
+        sortedNums = sorted(nums)
+        curr = 0
+        numToGroup = {}
+        numToGroup[sortedNums[0]] = curr
+
+        groupToList = {}
+        groupToList[curr] = deque([sortedNums[0]])
+
         for i in range(1, n):
-            if enum[i][0] - enum[i - 1][0] <= limit:
-                groups[-1].append(enum[i])
-            else:
-                groups.append([enum[i]])
-        for group in groups:
-            idxs = sorted([i[1] for i in group])
-            for i in range(len(group)):
-                nums[idxs[i]] = group[i][0]
+            if sortedNums[i]-sortedNums[i-1]>limit:
+                curr+=1
+            numToGroup[sortedNums[i]] = curr
+
+            if curr not in groupToList:
+                groupToList[curr] = deque()
+            groupToList[curr].append(sortedNums[i])
+        
+        for i in range(n):
+            num = nums[i]
+            group = numToGroup[num]
+            nums[i] = groupToList[group].popleft()
+        
         return nums
+            
+
+
+
