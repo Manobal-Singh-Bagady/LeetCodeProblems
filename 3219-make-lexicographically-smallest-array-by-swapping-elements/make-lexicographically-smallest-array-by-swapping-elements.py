27fa6dd428e78e45dce010b129e917a26d8c4141
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-25 22:33:24 +0530

    Time: 367 ms (50.00%) | Memory: 46.8 MB (83.33%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,23 +1,39 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        n = len(nums)
-        indexed = []
-        for i in range(n):
-            indexed.append((nums[i],i))
-        indexed.sort(key=lambda x:x[0])
-        groups = [[indexed[0][1]]]
-        for i in range(1,n):
-            if indexed[i][0]-indexed[i-1][0]<=limit:
-                groups[-1].append(indexed[i][1])
-            else:
-                groups.append([indexed[i][1]])
+        # n = len(nums)
+        # indexed = []
+        # for i in range(n):
+        #     indexed.append((nums[i],i))
+        # indexed.sort(key=lambda x:x[0])
+        # groups = [[indexed[0][1]]]
+        # for i in range(1,n):
+        #     if indexed[i][0]-indexed[i-1][0]<=limit:
+        #         groups[-1].append(indexed[i][1])
+        #     else:
+        #         groups.append([indexed[i][1]])
         
+        # for group in groups:
+        #     sortedVals = sorted([nums[i] for i in group])
+        #     sortedGroup = sorted(group)
+        #     for i in range(len(group)):
+        #         nums[sortedGroup[i]]=sortedVals[i]
+        # return nums
+
+        n = len(nums)
+        enum = sorted((num,i) for i, num in enumerate(nums))
+        groups = [[enum[0]]]
+        for i in range(1,n):
+            if enum[i][0]-enum[i-1][0]<=limit:
+                groups[-1].append(enum[i])
+            else:
+                groups.append([enum[i]])
         for group in groups:
-            sortedVals = sorted([nums[i] for i in group])
-            sortedGroup = sorted(group)
+            vals = sorted([i[0] for i in group])
+            idxs = sorted([i[1] for i in group])
             for i in range(len(group)):
-                nums[sortedGroup[i]]=sortedVals[i]
+                nums[idxs[i]]=vals[i]
         return nums
+
 
 
         
