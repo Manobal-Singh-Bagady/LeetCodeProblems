8c2b188194b0b17f56fcaef3c5af6e0199584954
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-26 23:21:35 +0530

    Time: 120 ms (99.49%) | Memory: 31.6 MB (67.69%) - LeetSync

--- 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
@@ -3,36 +3,35 @@ class Solution:
         n = len(fav)
         longestCycle = 0
         twoCycleInvitations = 0
-        inDegree = [0]*n
-        depth = [1]*n
+        inDegree = [0] * n
+        depth = [1] * n
 
         for i in fav:
-            inDegree[i]+=1
-        q = deque(i for i in range(n) if inDegree[i]==0)
-        
+            inDegree[i] += 1
+        q = deque(i for i in range(n) if inDegree[i] == 0)
+
         while q:
             curr = q.popleft()
             next = fav[curr]
 
-            depth[next] = depth[curr]+1
-            inDegree[next]-=1
-            if (inDegree[next]==0):
+            depth[next] = depth[curr] + 1
+            inDegree[next] -= 1
+            if inDegree[next] == 0:
                 q.append(next)
 
         for i in range(n):
-            if inDegree[i]==0: continue
+            if inDegree[i] == 0:
+                continue
             cycleLength = 1
-            inDegree[i]=0
+            inDegree[i] = 0
             curr = fav[i]
-            while (curr!=i):
+            while curr != i:
                 inDegree[curr] = 0
-                cycleLength+=1
+                cycleLength += 1
                 curr = fav[curr]
-            if cycleLength==2:
-                twoCycleInvitations+=depth[i]+depth[fav[i]]
+            if cycleLength == 2:
+                twoCycleInvitations += depth[i] + depth[fav[i]]
             else:
                 longestCycle = max(longestCycle, cycleLength)
-            
-        
+
         return max(longestCycle, twoCycleInvitations)
-        
