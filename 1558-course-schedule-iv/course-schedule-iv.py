93b4ecf733baff686364eb350e7e3cf85af7da29
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-27 22:08:19 +0530

    Time: 22 ms (93.53%) | Memory: 20.5 MB (47.37%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,32 +1,31 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        gr = [[] for _ in range(numCourses)]
+        gr = defaultdict(list)
         inDegree = [0]*numCourses
 
         for x, y in prerequisites:
             gr[x].append(y)
             inDegree[y]+=1
         
-        q = deque(i for i in range(numCourses) if inDegree[i]==0)
-        nodePre = [set() for _ in range(numCourses)]
+        q = deque(i for i, deg in enumerate(inDegree) if deg==0)
+
+        nodePre = defaultdict(set)
         while q:
             curr = q.popleft()
             for child in gr[curr]:
                 nodePre[child].add(curr)
-                for i in nodePre[curr]:
-                    nodePre[child].add(i)
+                nodePre[child].update(nodePre[curr])
                 inDegree[child]-=1
                 if inDegree[child]==0:
                     q.append(child)
-        
         ans = []
         for x, y in queries:
             if x in nodePre[y]:
                 ans.append(True)
             else:
                 ans.append(False)
-
         return ans
+
 
 
         
