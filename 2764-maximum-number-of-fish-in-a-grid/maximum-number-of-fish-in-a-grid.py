30295976b111053c8754b1e8f9812b7061911e7b
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-28 18:48:41 +0530

    Time: 16 ms (99.76%) | Memory: 17.9 MB (49.02%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -1,10 +1,13 @@
 class Solution:
     def countFishDFS(self, grid, i, j):
-        row, col = len(grid), len(grid[0])
-        if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] == 0:
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
             return 0
+        if grid[i][j] == 0:
+            return 0
+
         temp = grid[i][j]
         grid[i][j] = 0
+
         return (
             temp
             + self.countFishDFS(grid, i + 1, j)
@@ -14,11 +17,10 @@ class Solution:
         )
 
     def findMaxFish(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
         maxFish = 0
 
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] > 0:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] != 0:
                     maxFish = max(maxFish, self.countFishDFS(grid, i, j))
         return maxFish
