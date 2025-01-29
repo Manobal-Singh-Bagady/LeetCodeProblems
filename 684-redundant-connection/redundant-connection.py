3fae51b1ce8d66bdc464e7d1060473b7bdd98133
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-29 16:21:31 +0530

    Time: 3 ms (58.46%) | Memory: 18.1 MB (75.33%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,33 +1,36 @@
 class DSU:
     def __init__(self, n):
-        self.parent = list(range(n + 1))
-        self.size = [1] * (n + 1)
-
-    def find(self, node):
-        if self.parent[node] != node:
+        self.parent = list(range(n+1))
+        self.size = [1]*(n+1)
+    
+    def find(self,node):
+        if self.parent[node]!=node:
             self.parent[node] = self.find(self.parent[node])
         return self.parent[node]
-
+    
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
 
-        if x == y:
+        if x==y:
             return False
+        
+        xSize = self.size[x]
+        ySize = self.size[y]
 
-        if self.size[x] > self.size[y]:
-            self.parent[y] = x
-            self.size[y] += self.size[x]
+        if xSize>ySize:
+            self.parent[y]=x
+            self.size[y]+=self.size[x]
         else:
-            self.parent[x] = y
-            self.size[x] += y
+            self.parent[x]=y
+            self.size[x]+=y
         return True
-
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         dsu = DSU(len(edges))
-        for x, y in edges:
-            if not dsu.union(x, y):
-                return [x, y]
+
+        for x,y in edges:
+            if not dsu.union(x,y):
+                return [x,y]
         return []
