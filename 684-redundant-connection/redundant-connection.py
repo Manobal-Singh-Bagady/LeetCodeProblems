740bd8c9367b26adc46eb4dc0ab48411ee9d86a0
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-29 14:06:14 +0530

    Time: 14 ms (14.53%) | Memory: 18.1 MB (61.58%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isConnected(self, src, target, visited, gr):
+        visited[src]=True
+
+        if src==target:
+            return True
+
+        found = False
+        for node in gr[src]:
+            if not visited[node]:
+                found = found or self.isConnected(node, target, visited,gr)
+        return found
+
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        gr = [[] for _ in range(n+1)]
+
+        for x, y in edges:
+            visited = [False]*(n+1)
+
+            if self.isConnected(x, y, visited, gr):
+                return [x,y]
+            
+            gr[x].append(y)
+            gr[y].append(x)
+        return []
+        
