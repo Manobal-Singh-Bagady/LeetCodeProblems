4ee6154f753f21a37bb6f03821ac1e5bf14c9535
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-01-29 16:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (22.29%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,26 +1,36 @@
-class Solution:
-    def dfs(self, src, target, visited, gr):
-        visited[src]=True
-        if src==target:
-            return True
-        found = False
-        for node in gr[src]:
-            if not visited[node]:
-                found |= self.dfs(node, target, visited, gr)
-        return found
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n+1))
+        self.size = [1]*(n+1)
+    
+    def find(self,node):
+        if self.parent[node]!=node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        gr = [[] for _ in range(n+1)]
-
-        for x, y in edges:
-            visited = [False]*(n+1)
-            if self.dfs(x,y,visited,gr):
-                return [x,y]
-            gr[x].append(y)
-            gr[y].append(x)
-        return []
-
-
+        if x==y:
+            return False
         
+        xSize = self.size[x]
+        ySize = self.size[y]
+
+        if xSize>ySize:
+            self.parent[y]=x
+            self.size[y]+=self.size[x]
+        else:
+            self.parent[x]=y
+            self.size[x]+=y
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        dsu = DSU(len(edges))
+
+        for x,y in edges:
+            if not dsu.union(x,y):
+                return [x,y]
+        return []
