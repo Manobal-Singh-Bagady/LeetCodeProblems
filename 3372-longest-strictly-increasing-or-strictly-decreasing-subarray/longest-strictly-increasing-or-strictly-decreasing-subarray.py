5d8f3dc9087b8596b00b18ae911233b178b1557c
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-03 18:08:30 +0530

    Time: 2 ms (59.22%) | Memory: 17.6 MB (67.96%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -3,15 +3,15 @@ class Solution:
         maxLen = 1
         inc = 1
         dec = 1
-        for i in range(len(nums)-1):
-            if nums[i]>nums[i+1]:
-                dec+=1
-                inc=1
-            elif nums[i]<nums[i+1]:
-                inc+=1
-                dec=1
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                dec += 1
+                inc = 1
+            elif nums[i] < nums[i + 1]:
+                inc += 1
+                dec = 1
             else:
-                inc=1
-                dec=1
+                inc = 1
+                dec = 1
             maxLen = max(maxLen, inc, dec)
         return maxLen
