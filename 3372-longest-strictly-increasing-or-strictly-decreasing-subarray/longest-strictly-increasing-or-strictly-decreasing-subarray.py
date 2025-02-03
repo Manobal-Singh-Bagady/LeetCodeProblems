1441d0cc76164102213c2e0c95e0b7ddfd491548
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-03 18:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.96%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -4,12 +4,12 @@ class Solution:
         inc = 1
         dec = 1
         for i in range(len(nums)-1):
-            if nums[i]<nums[i+1]:
-                inc+=1
-                dec=1
-            elif nums[i]>nums[i+1]:
+            if nums[i]>nums[i+1]:
                 dec+=1
                 inc=1
+            elif nums[i]<nums[i+1]:
+                inc+=1
+                dec=1
             else:
                 inc=1
                 dec=1
