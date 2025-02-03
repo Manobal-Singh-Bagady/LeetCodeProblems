b3b42f73beb76ca4f38b68d2e51fb3d5ea9acba0
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-03 18:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.50%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,7 +1,6 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        max_inc = 1
-        max_dec = 1
+        maxLen = 1
         curr_inc = 1
         curr_dec = 1
         for i in range(len(nums)-1):
@@ -9,11 +8,9 @@ class Solution:
                 curr_dec+=1
             else:
                 curr_dec=1
-            max_dec = max(max_dec, curr_dec)
             if nums[i]<nums[i+1]:
                 curr_inc+=1
             else:
                 curr_inc=1
-            max_inc=max(max_inc,curr_inc)
-            
-        return max(max_inc, max_dec)
+            maxLen = max(maxLen, curr_inc, curr_dec)
+        return maxLen
