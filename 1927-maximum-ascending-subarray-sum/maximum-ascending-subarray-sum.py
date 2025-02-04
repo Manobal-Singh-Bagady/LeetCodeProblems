78f3a8e839ce8212e4ca4c8a092d967d4d43cd95
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-04 20:50:59 +0530

    Time: 1 ms (16.03%) | Memory: 17.7 MB (67.60%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.py
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.py
@@ -3,10 +3,9 @@ class Solution:
         ans = nums[0]
         curr = nums[0]
         for i in range(1, len(nums)):
-            if nums[i]>nums[i-1]:
-                curr+=nums[i]
+            if nums[i] > nums[i - 1]:
+                curr += nums[i]
             else:
-                curr=nums[i]
-            ans=max(ans,curr)
+                curr = nums[i]
+            ans = max(ans, curr)
         return ans
-        
