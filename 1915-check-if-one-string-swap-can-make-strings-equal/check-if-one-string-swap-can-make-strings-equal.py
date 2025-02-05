b010e744168d4a85f0604503d1f8c738a4b4cbde
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-05 15:59:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.15%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -1,16 +1,12 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        if s1==s2:
+        if s1 == s2:
             return True
 
-        diff = 0
-        for i in range(len(s1)):
-            if s1[i]!=s2[i]:
-                diff+=1
-                if diff>2:
-                    return False
-        
-        if Counter(s1)!=Counter(s2):
+        diff = sum(1 for i in range(len(s1)) if s1[i] != s2[i])
+        if diff > 2:
+            return False
+
+        if Counter(s1) != Counter(s2):
             return False
         return True
-            
