2b920fa48152c6b061ab424f5fefcfff1fbd389b
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-05 15:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.15%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -5,16 +5,13 @@ class Solution:
         diff = 0
         for i in range(n):
             if s1[i] != s2[i]:
-                diff+=1
+                diff += 1
                 if not chars:
                     chars.append(s1[i])
                     chars.append(s2[i])
                 else:
-                    if chars[0]!=s2[i] or chars[1]!=s1[i]:
+                    if chars[0] != s2[i] or chars[1] != s1[i]:
                         return False
-        if len(chars)>2:
-            return False
-        if diff==2 or diff==0:
+        if diff == 2 or diff == 0:
             return True
         return False
-        
