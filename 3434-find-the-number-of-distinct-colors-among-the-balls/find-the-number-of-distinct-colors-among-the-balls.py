9b727f9d5ad251ad9a4cfff896e60b23ff79444d
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-07 23:52:20 +0530

    Time: 59 ms (96.25%) | Memory: 63 MB (87.82%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -10,7 +10,6 @@ class Solution:
                 prevColor = balls[ball]
                 colors[prevColor] -= 1
                 if colors[prevColor] == 0:
-                    del colors[prevColor]
                     distinct -= 1
             balls[ball] = color
             if color in colors:
