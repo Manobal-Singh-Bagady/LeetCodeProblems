a3d70d532534e6dc22efc6f825a394c2f73a042c
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-07 23:33:56 +0530

    Time: 65 ms (81.50%) | Memory: 63.1 MB (58.08%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -3,14 +3,19 @@ class Solution:
         balls = {}
         colors = {}
         ans = []
+        distinct = 0
         for ball, color in queries:
             if ball in balls:
                 prevColor = balls[ball]
                 colors[prevColor] -= 1
                 if colors[prevColor] == 0:
-                    colors.pop(prevColor)
-            colors[color] = colors.get(color, 0) + 1
+                    del colors[prevColor]
+                    distinct -= 1
+            if color not in colors:
+                colors[color] = 0
+                distinct += 1
+            colors[color] += 1
             balls[ball] = color
-            ans.append(len(colors))
+            ans.append(distinct)
 
         return ans
