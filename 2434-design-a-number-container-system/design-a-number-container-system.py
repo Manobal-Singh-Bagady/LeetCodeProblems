1da0712d3d264d5bed1c84693969807c958aee22
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-09 00:07:48 +0530

    Time: 113 ms (76.96%) | Memory: 76.5 MB (75.98%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -3,27 +3,25 @@ class NumberContainers:
     def __init__(self):
         self.container = {}
         self.numbers = {}
-        
 
     def change(self, index: int, number: int) -> None:
-        self.container[index]=number
+        self.container[index] = number
         if number not in self.numbers:
-            self.numbers[number]=[]
-        heapq.heappush(self.numbers[number],index)
-        
+            self.numbers[number] = []
+        heapq.heappush(self.numbers[number], index)
 
     def find(self, number: int) -> int:
         if number not in self.numbers:
             return -1
-        
+
         heap = self.numbers[number]
         while heap:
             index = heap[0]
-            if self.container[index]==number:
+            if self.container[index] != number:
+                heapq.heappop(heap)
+            else:
                 return index
-            heapq.heappop(heap)
         return -1
-        
 
 
 # Your NumberContainers object will be instantiated and called as such:
