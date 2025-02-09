7cc08a02fd8eb6b11031aec3a4344402ada77366
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-09 22:47:55 +0530

    Time: 55 ms (97.14%) | Memory: 39.3 MB (8.10%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,8 +1,11 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        badPairs = 0
-        diffCount = {}
-        for i in range(len(nums)):
-            badPairs += i - (goodPairs := diffCount.get((diff := nums[i] - i), 0))
-            diffCount[diff] = goodPairs + 1
-        return badPairs
+        n = len(nums)
+        arr = [nums[i]-i for i in range(n)]
+        count = Counter(arr)
+        totalComb = n*(n-1)/2
+        for i in count.values():
+            if i!=1:
+                totalComb-=i*(i-1)/2
+        return int(totalComb)
+
