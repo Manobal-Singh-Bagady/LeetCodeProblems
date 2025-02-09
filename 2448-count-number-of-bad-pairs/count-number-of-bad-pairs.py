9dbdb59dcccdca124614dd46ddf9cd94f2209c90
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-09 22:53:15 +0530

    Time: 55 ms (97.14%) | Memory: 39.4 MB (8.10%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        n = len(nums)
-        arr = [nums[i]-i for i in range(n)]
-        count = Counter(arr)
-        totalComb = n*(n-1)/2
-        for i in count.values():
-            if i!=1:
-                totalComb-=i*(i-1)/2
-        return int(totalComb)
+        diffArr = [num - i for i, num in enumerate(nums)]
+        counts = Counter(diffArr)
 
+        n = len(nums)
+        totalCombinations = n * (n - 1) // 2
+        for count in counts.values():
+            if count > 1:
+                totalCombinations -= count * (count - 1) // 2
+        return totalCombinations
