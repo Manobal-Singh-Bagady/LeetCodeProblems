4a93c750661d2dd6bfd545041f37e4c7e9d176b2
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-09 22:54:18 +0530

    Time: 51 ms (99.76%) | Memory: 39.3 MB (11.67%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -4,8 +4,10 @@ class Solution:
         counts = Counter(diffArr)
 
         n = len(nums)
-        totalCombinations = n * (n - 1) // 2
+        totalCombinations = n * (n - 1) // 2  # nC2 total combinations
         for count in counts.values():
             if count > 1:
-                totalCombinations -= count * (count - 1) // 2
+                totalCombinations -= (
+                    count * (count - 1) // 2
+                )  # nC2 combinations are subtracted which are good pairs
         return totalCombinations
