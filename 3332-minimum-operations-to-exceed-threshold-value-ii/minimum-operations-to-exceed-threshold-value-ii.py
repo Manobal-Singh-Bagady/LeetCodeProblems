734f9ebfde5ac73a14a0972bb9c40a05a79dd76f
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-14 03:50:23 +0530

    Time: 187 ms (95.51%) | Memory: 35.5 MB (72.65%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        heapq.heapify(nums)
+        heapify(nums)
         count = 0
         while nums[0] < k:
-            x = heapq.heappop(nums)
-            y = heapq.heappop(nums)
-            heapq.heappush(nums, x * 2 + y)
+            # Since x is popped before y, x <= y.
+            heapq.heappush(nums, heappop(nums) * 2 + heappop(nums))
             count += 1
         return count
