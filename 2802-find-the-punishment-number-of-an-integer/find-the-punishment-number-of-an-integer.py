b76fba534fe7fc0b589d807ca1d22abace8a821c
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-16 02:27:58 +0530

    Time: 847 ms (67.43%) | Memory: 17.9 MB (55.14%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -3,8 +3,10 @@ class Solution:
         if not square:
             return num == 0
 
+        sum = 0
         for i in range(len(square)):
-            if (sum := int(square[: i + 1])) > num:
+            sum = sum * 10 + int(square[i])
+            if sum > num:
                 break
             if self.canPartition(square[i + 1 :], num - sum):
                 return True
@@ -13,7 +15,7 @@ class Solution:
     def punishmentNumber(self, n: int) -> int:
         ans = 0
         for i in range(1, n + 1):
-            squared = i*i
+            squared = i * i
             if self.canPartition(str(squared), i):
                 ans += squared
         return ans
