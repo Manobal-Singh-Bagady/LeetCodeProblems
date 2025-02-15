11d4b6ce7a8a080061b013cf0b6d75efc206168d
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-16 02:39:03 +0530

    Time: 90 ms (83.43%) | Memory: 17.5 MB (99.43%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -15,10 +15,24 @@ class Solution:
                 return True
         return False
 
+    def canPartition2(self, square, num):
+        if num < 0 or square < num:
+            return False
+
+        if square == num:
+            return True
+
+        return (
+            self.canPartition2(square // 10, num - square % 10)
+            or self.canPartition2(square // 100, num - square % 100)
+            or self.canPartition2(square // 1000, num - square % 1000)
+        )
+
     def punishmentNumber(self, n: int) -> int:
         ans = 0
         for i in range(1, n + 1):
             squared = i * i
-            if self.canPartition(str(squared), i):
+            # if self.canPartition(str(squared), i):
+            if self.canPartition2(squared, i):
                 ans += squared
         return ans
