bbfcf34a6baa89f6a144d37edb67c3d9b1087200
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-16 23:52:22 +0530

    Time: 3 ms (95.31%) | Memory: 18 MB (21.88%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -2,26 +2,25 @@ class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
         ans = [0] * (2 * n - 1)
         used = [False] * (n + 1)
-        self.generateSequence(ans, used, 0, n)
-        return ans
 
-    def generateSequence(
-        self, sequence: List[int], used: List[bool], index: int, n: int
-    ) -> bool:
-        if index == (2 * n - 1):
-            return True
-        if sequence[index] != 0:
-            return self.generateSequence(sequence, used, index + 1, n)
-        for num in range(n, 0, -1):
-            if used[num]:
-                continue
-            nextIndex = index if num == 1 else index + num
-            if num > 1 and (nextIndex >= (2 * n - 1) or sequence[nextIndex] != 0):
-                continue
-            sequence[index] = sequence[nextIndex] = num
-            used[num] = True
-            if self.generateSequence(sequence, used, index + 1, n):
+        def generateSeq(i: int) -> bool:
+            if i == (2 * n - 1):
                 return True
-            sequence[index] = sequence[nextIndex] = 0
-            used[num] = False
-        return False
+            if ans[i]:
+                return generateSeq(i + 1)
+            for num in range(n, 0, -1):
+                if used[num]:
+                    continue
+                nextIndex = i if num == 1 else num + i
+                if num > 1 and (nextIndex >= (2 * n - 1) or ans[nextIndex] != 0):
+                    continue
+                ans[i] = ans[nextIndex] = num
+                used[num] = True
+                if generateSeq(i + 1):
+                    return True
+                ans[i] = ans[nextIndex] = 0
+                used[num] = False
+            return False
+
+        generateSeq(0)
+        return ans
