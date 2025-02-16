1843e10f12af957f71cd77d614cc53ac7209a0e2
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-17 00:00:22 +0530

    Time: 2 ms (98.44%) | Memory: 18 MB (21.88%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -1,28 +1,27 @@
 class Solution:
+    def generateSequence(
+        self, sequence: List[int], used: List[bool], index: int, n: int
+    ) -> bool:
+        if index == len(sequence):
+            return True
+        if sequence[index] != 0:
+            return self.generateSequence(sequence, used, index + 1, n)
+        for num in range(n, 0, -1):
+            if used[num]:
+                continue
+            nextIndex = index if num == 1 else num + index
+            if num > 1 and (nextIndex >= len(sequence) or sequence[nextIndex] != 0):
+                continue
+            sequence[index] = sequence[nextIndex] = num
+            used[num] = True
+            if self.generateSequence(sequence, used, index + 1, n):
+                return True
+            sequence[index] = sequence[nextIndex] = 0
+            used[num] = False
+        return False
+
     def constructDistancedSequence(self, n: int) -> List[int]:
         ans = [0] * (2 * n - 1)
-        used = set()
-        def generateSeq(i: int) -> bool:
-            if i == len(ans):
-                return True
-            if ans[i]:
-                return generateSeq(i + 1)
-            for num in range(n, 0, -1):
-                if num in used:
-                    continue
-                used.add(num)
-                ans[i] = num
-                if num == 1:
-                    if generateSeq(i + 1):
-                        return True
-                elif num + i < len(ans) and ans[num + i] == 0:
-                    ans[num + i] = num
-                    if generateSeq(i + 1):
-                        return True
-                    ans[num + i] = 0
-                ans[i] = 0
-                used.remove(num)
-            return False
-
-        generateSeq(0)
+        used = [False] * (n + 1)
+        self.generateSequence(ans, used, 0, n)
         return ans
