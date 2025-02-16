59f341dcda8bf7471ad666fbc92baef3e18f3083
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-16 23:16:24 +0530

    Time: 5 ms (56.25%) | Memory: 17.9 MB (21.88%) - LeetSync

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def generateSequence(
+        self, sequence: List[int], used: List[bool], index: int, n: int
+    ) -> bool:
+        if index == len(sequence):
+            return True
+        if sequence[index] != 0:
+            return self.generateSequence(sequence, used, index + 1, n)
+        for num in range(n, 0, -1):
+            nextIndex = index if num == 1 else num + index
+            if used[num] or (
+                num > 1 and (nextIndex >= len(sequence) or sequence[nextIndex] != 0)
+            ):
+                continue
+            sequence[index] = sequence[nextIndex] = num
+            used[num] = True
+            if self.generateSequence(sequence, used, index + 1, n):
+                return True
+            sequence[index] = sequence[nextIndex] = 0
+            used[num] = False
+        return False
+
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        ans = [0] * (2 * n - 1)
+        used = [False] * (n + 1)
+        self.generateSequence(ans, used, 0, n)
+        return ans
