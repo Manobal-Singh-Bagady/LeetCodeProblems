5d597ef39706bb7712423ff0b5c8424594369394
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-17 22:31:44 +0530

    Time: 40 ms (48.78%) | Memory: 17.8 MB (83.06%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,16 +1,15 @@
 class Solution:
-    def genSeq(self, curr: str, seq: set, used: List[bool], tiles: str):
-        seq.add(curr)
-        for i in range(len(tiles)):
-            if not used[i] and curr+tiles[i] not in seq:
-                used[i] = True
-                self.genSeq(curr + tiles[i], seq, used, tiles)
-                used[i] = False
+    def genCount(self, freq: Dict[str, int]):
+        total = 0
+        for i in freq:
+            if freq[i] == 0:
+                continue
+            total += 1
+            freq[i] -= 1
+            total += self.genCount(freq)
+            freq[i] += 1
+        return total
 
     def numTilePossibilities(self, tiles: str) -> int:
-        seq = set()
-        used = [False] * len(tiles)
-
-        self.genSeq("", seq, used, tiles)
-
-        return len(seq) - 1
+        count = Counter(tiles)
+        return self.genCount(count)
