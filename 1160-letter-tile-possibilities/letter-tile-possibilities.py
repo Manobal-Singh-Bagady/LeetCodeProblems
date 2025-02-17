39eb8440565b1939674b81e875d51a0f84a1b5dc
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-17 23:56:42 +0530

    Time: 2 ms (98.37%) | Memory: 18.1 MB (45.94%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,22 +1,29 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         seen = set()
+        return self.genSeq("", 0, seen, "".join(sorted(tiles))) - 1
 
-        tiles = "".join(sorted(tiles))
-
-        return self.genSeq(tiles, "", 0, seen) - 1
-
-    def genSeq(self, tiles: str, curr: str, pos: int, seen: Set[str]) -> int:
+    def genSeq(self, curr: str, pos: int, seen: set, tiles: str) -> int:
         if pos >= len(tiles):
             if curr not in seen:
                 seen.add(curr)
                 return self.perms(curr)
             return 0
-        return self.genSeq(tiles, curr, pos + 1, seen) + self.genSeq(
-            tiles, curr + tiles[pos], pos + 1, seen
+        return self.genSeq(curr, pos + 1, seen, tiles) + self.genSeq(
+            curr + tiles[pos], pos + 1, seen, tiles
         )
 
     def perms(self, string: str) -> int:
+        """
+            n!
+        ---------
+        n1! * n2!
+
+        where,
+        n = total no of tiles
+        n1 = freq of 1st tile
+        n2 = freq of 2nd tile
+        """
         total = factorial(len(string))
         for count in Counter(string).values():
             total //= factorial(count)
