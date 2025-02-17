eae2196c970717b90a39f9b799fd4ce7fbd5bfd1
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-17 23:40:40 +0530

    Time: 4 ms (95.80%) | Memory: 17.8 MB (67.21%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,15 +1,23 @@
 class Solution:
-    def genCount(self, freq: Dict[str, int]):
-        total = 0
-        for i in freq:
-            if freq[i] == 0:
-                continue
-            total += 1
-            freq[i] -= 1
-            total += self.genCount(freq)
-            freq[i] += 1
-        return total
-
     def numTilePossibilities(self, tiles: str) -> int:
-        count = Counter(tiles)
-        return self.genCount(count)
+        seen = set()
+
+        tiles = "".join(sorted(tiles))
+
+        return self.genSeq(tiles, "", 0, seen) - 1
+
+    def genSeq(self, tiles: str, curr: str, pos: int, seen: Set[str]) -> int:
+        if pos >= len(tiles):
+            if curr not in seen:
+                seen.add(curr)
+                return self.perms(curr)
+            return 0
+        return self.genSeq(tiles, curr, pos + 1, seen) + self.genSeq(
+            tiles, curr + tiles[pos], pos + 1, seen
+        )
+
+    def perms(self, string: str) -> int:
+        total = factorial(len(string))
+        for count in Counter(string).values():
+            total //= factorial(count)
+        return total
