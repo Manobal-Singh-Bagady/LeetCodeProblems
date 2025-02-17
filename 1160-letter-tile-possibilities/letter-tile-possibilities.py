eb4717917826d169773c3408a351f2d927c7f71d
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-17 22:22:36 +0530

    Time: 65 ms (21.00%) | Memory: 19.3 MB (32.66%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -0,0 +1,16 @@
+class Solution:
+    def genSeq(self, curr: str, seq: set, used: List[bool], tiles: str):
+        seq.add(curr)
+        for i in range(len(tiles)):
+            if not used[i]:
+                used[i] = True
+                self.genSeq(curr + tiles[i], seq, used, tiles)
+                used[i] = False
+
+    def numTilePossibilities(self, tiles: str) -> int:
+        seq = set()
+        used = [False] * len(tiles)
+
+        self.genSeq("", seq, used, tiles)
+
+        return len(seq) - 1
