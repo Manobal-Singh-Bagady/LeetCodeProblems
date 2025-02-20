54a45e50b376304a3f83c5989dc2804ce384cd85
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-20 20:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.03%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,11 +1,7 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        count = dict()
-        n = len(nums[0])
-        for i in range((2**n)):
-            count[str(bin(i))[2:].zfill(n)] = 0
-        for i in nums:
-            count[i] += 1
-        for i in count:
-            if count[i] == 0:
-                return i
+        ans = []
+        for i, num in enumerate(nums):
+            curr = num[i]
+            ans.append("1" if curr == "0" else "0")
+        return "".join(ans)
