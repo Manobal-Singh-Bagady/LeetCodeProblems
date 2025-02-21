d87d2f66192f921cc9a30b05e9380433151504c2
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-02-22 00:00:08 +0530

    Time: 15 ms (21.93%) | Memory: 22.3 MB (10.70%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -6,16 +6,15 @@
 #         self.right = right
 class FindElements:
     def __init__(self, root: Optional[TreeNode]):
-        seen = set()
+        self.seen = set()
         q = deque([(root, 0)])
         while q:
             curr, val = q.popleft()
-            seen.add(val)
+            self.seen.add(val)
             if curr.left:
                 q.append((curr.left, 2 * val + 1))
             if curr.right:
                 q.append((curr.right, 2 * val + 2))
-        self.seen = seen
 
     def find(self, target: int) -> bool:
         return target in self.seen
