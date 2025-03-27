f91b8a3cc8a944f4c649e096489f6bd22b96c4ae
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-03-27 22:07:37 +0530

    Time: 150 ms (14.36%) | Memory: 34.8 MB (13.86%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,14 +1,14 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        newCount = Counter()
-        major = max(count, key=count.get)
         n = len(nums)
+        count, newCount = Counter(nums), Counter()
+        major = max(count, key=count.get)
         for i, num in enumerate(nums):
             newCount[num] += 1
             count[num] -= 1
-            if count.get(major, 0) > (n - i - 1) // 2 and newCount.get(major, 0) > (
-                (i + 1) // 2
+            if (
+                count.get(major, 0) > (n - (i + 1)) // 2
+                and newCount.get(major, 0) > (i + 1) // 2
             ):
                 return i
         return -1
