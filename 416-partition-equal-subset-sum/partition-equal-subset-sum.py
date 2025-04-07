b524a00a06ba9aa13fce7303cfa600812c83f711
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-04-07 23:50:04 +0530

    Time: 380 ms (82.82%) | Memory: 19.6 MB (55.27%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,16 +1,11 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
-        if s%2!=0:
+        if s & 1:
             return False
-        dp = set()
-        dp.add(0)
+        dp = set([0])
 
         for i in nums:
-            dp.update([i+num for num in dp])
-        if s//2 in dp:
-            return True
-        return False
+            dp.update([i + num for num in dp])
 
-
-        
+        return s // 2 in dp
