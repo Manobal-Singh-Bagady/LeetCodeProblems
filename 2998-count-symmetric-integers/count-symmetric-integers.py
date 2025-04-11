e79ba09a59cfeacf4d7c298bbe450df2ac551857
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-04-12 02:28:12 +0530

    Time: 108 ms (96.33%) | Memory: 17.7 MB (81.43%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.py
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -1,15 +1,13 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         ans = 0
-        for i in range(low, high+1):
-            if i<100 and i%11==0:
-                ans+=1
-            if 1000<=i<10000:
-                left = i//1000 + i%1000//100
-                right=i%100//10 + i%10
-                if left==right:
-                    ans+=1
+        for i in range(low, high + 1):
+            if i < 100 and i % 11 == 0:
+                ans += 1
+            if 1000 <= i < 10000:
+                left = i // 1000 + i % 1000 // 100
+                right = i % 100 // 10 + i % 10
+                if left == right:
+                    ans += 1
 
         return ans
-            
-        
