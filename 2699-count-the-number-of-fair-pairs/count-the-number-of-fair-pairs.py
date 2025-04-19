9a8659448bb3902302e35a6a7865b2b483afe424
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-04-19 23:07:26 +0530

    Time: 243 ms (43.38%) | Memory: 31.7 MB (19.86%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        count = 0
+        for i in range(len(nums)):
+            left = bisect_left(nums, lower-nums[i], i+1)
+            right = bisect_right(nums, upper-nums[i],i+1)-1
+            count+=right-left+1
+        return count
+        
