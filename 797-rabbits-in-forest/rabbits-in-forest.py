2bc9db76908850c70dc5d54ee48e08186992cf3b
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-04-20 14:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (29.10%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -3,7 +3,7 @@ class Solution:
         count = Counter(answers)
         ans = 0
         for others in count:
-            group_size = others +1
-            num_groups = math.ceil(count[others]/group_size)
+            group_size = others + 1
+            num_groups = math.ceil(count[others] / group_size)
             ans += num_groups * group_size
         return ans
