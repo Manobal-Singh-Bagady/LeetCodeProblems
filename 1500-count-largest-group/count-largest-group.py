4fa67b7e1a5d22bcc92614935b95a68612cb979f
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2025-04-24 00:31:50 +0530

    Time: 23 ms (81.75%) | Memory: 17.8 MB (53.00%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -2,15 +2,12 @@ class Solution:
     def countDigits(self, n):
         count = 0
         while n:
-            count += n%10
-            n//=10
+            count += n % 10
+            n //= 10
         return count
 
     def countLargestGroup(self, n: int) -> int:
         mpp = defaultdict(int)
-        for i in range(1, n+1):
-            mpp[self.countDigits(i)]+=1
-        values = list(mpp.values())
-        maxCount = max(values)
-        return values.count(maxCount)
-        
+        for i in range(1, n + 1):
+            mpp[self.countDigits(i)] += 1
+        return list(mpp.values()).count(max(mpp.values()))
